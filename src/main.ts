0fbdfcd5d44123bf0e15979e2c4811770e9c39c2
Nic Makomva <nicholasstm@gmail.com>
2024-04-04 12:15:43 +0100

    rendered cards

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
 import "./styles/style.scss";
+import pokemonArray from "./data/pokemon";
+import { Pokemon } from "./data/types";
+
+const pokeContainer = document.querySelector(".card-container");
+const filterInput = document.querySelector<HTMLInputElement>(".search-box");
+
+if (!pokeContainer || !filterInput) {
+  throw new Error("Issue with selector");
+}
+
+const RenderPokeDeck = () => {
+  let pokeCards = "";
+  pokemonArray.forEach((pokemon) => {
+    pokeCards += `<div class='card'>
+      <img class='card__image' src='${pokemon.sprite}'></img>
+      <h1 class='card__heading'>${pokemon.name}</heading>
+      <p class='card__content'>${pokemon.types}</content>
+      <p class='card__text'>${pokemon.text}</text>
+     </div>
+     `;
+  });
+
+  pokeContainer.innerHTML = pokeCards;
+};
+
+RenderPokeDeck();
+
+
+
+
